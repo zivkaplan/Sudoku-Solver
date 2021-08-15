9c000dc733e7c15c12171043cfdc428dc125eb00
Shaya94 <yginzburg94@gmail.com>
2021-08-15 17:49:05 +0300

    creating basic html and connecting to js functions

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-(function sudoku() {
-    const board = [
-        [null, 5, 4, 6, 7, 8, 9, 1, 2], //0
-        [6, null, 2, 1, 9, 5, 3, 4, 8], //1
-        [1, 9, null, 3, 4, 2, 5, 6, 7], //2
+const sudoku = function (board) {
+    // const board = [
+    //     [null, 5, 4, 6, 7, 8, 9, 1, 2], //0
+    //     [6, null, 2, 1, 9, 5, 3, 4, 8], //1
+    //     [1, 9, null, 3, 4, 2, 5, 6, 7], //2
 
-        [8, 5, 9, null, 6, 1, 4, 2, 3], //3
-        [4, 2, 6, 8, null, 3, 7, 9, 1], //4
-        [7, 1, 3, 9, 2, null, 8, 5, 6], //5
+    //     [8, 5, 9, null, 6, 1, 4, 2, 3], //3
+    //     [4, 2, 6, 8, null, 3, 7, 9, 1], //4
+    //     [7, 1, 3, 9, 2, null, 8, 5, 6], //5
 
-        [9, 6, 1, 5, 3, 7, null, 8, 4], //6
-        [2, 8, 7, 4, 1, 9, 6, null, 5], //7
-        [3, 4, 5, 2, 8, 6, 1, 7, null], //8
-    ];
-
+    //     [9, 6, 1, 5, 3, 7, null, 8, 4], //6
+    //     [2, 8, 7, 4, 1, 9, 6, null, 5], //7
+    //     [3, 4, 5, 2, 8, 6, 1, 7, null], //8
+    // ];
     function checkRow(currentCell, num) {
         for (let i = 0; i < 9; i++) {
-            if (board[currentCell.x][i] == num && i !== currentCell.y) {
+            if (board[currentCell.x][i] === num && i !== currentCell.y) {
                 return false;
             }
         }
@@ -24,7 +23,7 @@
 
     function checkCol(currentCell, num) {
         for (let i = 0; i < 9; i++) {
-            if (board[i][currentCell.y] == num && i !== currentCell.x) {
+            if (board[i][currentCell.y] === num && i !== currentCell.x) {
                 return false;
             }
         }
@@ -52,13 +51,10 @@
         return checkRow(currentCell, num) && checkCol(currentCell, num) && checkSquare(currentCell, num);
     }
 
-    let cell = { x: 3, y: 3 };
-    let num = 7;
-
     function solve() {
         for (let x = 0; x < 9; x++) {
             for (let y = 0; y < 9; y++) {
-                if (board[x][y] === null) {
+                if (!board[x][y]) {
                     const cell = { x, y };
                     for (let num = 1; num <= 9; num++) {
                         if (validateCell(cell, num)) {
@@ -70,9 +66,39 @@
                 }
             }
         }
+        console.log(board);
         return true;
     }
+    return { solve };
+};
 
-    solve();
-    console.log(board);
+const buildGridBoard = (function () {
+    const board = document.querySelector(".board");
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const cell = document.createElement("input");
+            cell.classList.add("cell");
+            cell.setAttribute("type", "number");
+            cell.setAttribute("min", "0");
+            cell.setAttribute("max", "9");
+            cell.dataset.x = i;
+            cell.dataset.y = j;
+            cell.value = 0;
+            board.append(cell);
+        }
+    }
 })();
+
+const solveBtn = document.querySelector(".solve-btn");
+solveBtn.addEventListener("click", function (e) {
+    get2DArray();
+});
+
+function get2DArray() {
+    const board = [[], [], [], [], [], [], [], [], []];
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => (board[cell.dataset.x][cell.dataset.y] = parseInt(cell.value)));
+    console.log(board);
+    sudoku(board).solve();
+    console.log(board);
+}
