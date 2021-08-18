6913efdc68c2f4389e093d1a272ff8a7dd5e83fe
Ziv Kaplan <zivkaplan@gmail.com>
2021-08-18 12:30:51 +0300

    fixed validation (switched to array deep-copy)

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const sudoku = (function () {
-    let board;
-    let boardCopyForValidation;
+    const board = {
+        original: null,
+        copyForValidation: null,
+    };
 
     function checkRow(currentCell, num) {
         for (let i = 0; i < 9; i++) {
-            if (board[currentCell.x][i] === num && i !== currentCell.y) {
+            if (
+                board.original[currentCell.x][i] === num &&
+                i !== currentCell.y
+            ) {
                 return false;
             }
         }
@@ -13,7 +18,10 @@ const sudoku = (function () {
 
     function checkCol(currentCell, num) {
         for (let i = 0; i < 9; i++) {
-            if (board[i][currentCell.y] === num && i !== currentCell.x) {
+            if (
+                board.original[i][currentCell.y] === num &&
+                i !== currentCell.x
+            ) {
                 return false;
             }
         }
@@ -33,7 +41,7 @@ const sudoku = (function () {
         for (let i = squareRange.x.start; i <= squareRange.x.end; i++) {
             for (let j = squareRange.y.start; j <= squareRange.y.end; j++) {
                 if (
-                    board[i][j] === num &&
+                    board.original[i][j] === num &&
                     (i !== currentCell.x || j !== currentCell.y)
                 ) {
                     return false;
@@ -55,7 +63,7 @@ const sudoku = (function () {
     function findFullCellOnBoardCopyForValidation() {
         for (let x = 0; x < 9; x++) {
             for (let y = 0; y < 9; y++) {
-                if (boardCopyForValidation[x][y]) {
+                if (board.copyForValidation[x][y]) {
                     return { x, y };
                 }
             }
@@ -69,10 +77,10 @@ const sudoku = (function () {
         else if (
             validateCell(
                 fullCell,
-                boardCopyForValidation[fullCell.x][fullCell.y]
+                board.copyForValidation[fullCell.x][fullCell.y]
             )
         ) {
-            boardCopyForValidation[fullCell.x][fullCell.y] = '';
+            board.copyForValidation[fullCell.x][fullCell.y] = '';
             if (validateBoard()) return true;
         }
 
@@ -83,7 +91,7 @@ const sudoku = (function () {
     function findEmptyCell() {
         for (let x = 0; x < 9; x++) {
             for (let y = 0; y < 9; y++) {
-                if (!board[x][y]) {
+                if (!board.original[x][y]) {
                     return { x, y };
                 }
             }
@@ -96,28 +104,31 @@ const sudoku = (function () {
         if (!emptyCell) return true;
         for (let num = 1; num <= 9; num++) {
             if (validateCell(emptyCell, num)) {
-                board[emptyCell.x][emptyCell.y] = num;
+                board.original[emptyCell.x][emptyCell.y] = num;
                 if (solve()) {
                     return true;
                 }
             }
-            board[emptyCell.x][emptyCell.y] = '';
+            board.original[emptyCell.x][emptyCell.y] = '';
         }
         return false;
     }
 
     function setBoard(newBoard) {
         if (Array.isArray(newBoard)) {
-            board = newBoard.slice();
-            boardCopyForValidation = newBoard.slice();
+            board.original = JSON.parse(JSON.stringify(newBoard));
+            board.copyForValidation = JSON.parse(JSON.stringify(newBoard));
         }
     }
 
     function getBoard() {
-        return board;
+        return board.original;
     }
 
-    return { setBoard, getBoard, solve, validateBoard };
+    function getValidationBoard() {
+        return board.copyForValidation;
+    }
+    return { setBoard, getBoard, solve, validateBoard, getValidationBoard };
 })();
 
 const htmlGame = {
@@ -157,12 +168,12 @@ const htmlGame = {
         if (!sudoku.solve()) {
             return { status: false, data: 'Unsovable Board' };
         }
+        console.log(sudoku.getBoard(), sudoku.getValidationBoard());
         return { status: true, data: sudoku.getBoard() };
     },
 
     showSolution: function (board) {
         const cells = document.querySelectorAll('.cell');
-
         cells.forEach(
             (cell) => (cell.value = board[cell.dataset.x][cell.dataset.y])
         );
@@ -170,7 +181,6 @@ const htmlGame = {
 
     clearBoard: function () {
         const cells = document.querySelectorAll('.cell');
-
         cells.forEach((cell) => (cell.value = ''));
     },
 
